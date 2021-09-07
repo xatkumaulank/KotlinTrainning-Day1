da65a92761baba105dfc22a8ce02fa237ffdd9d6
xatkumaulank <70027310+xatkumaulank@users.noreply.github.com>
2021-09-07 15:37:34 +0700

    open keyword

--- src/main/kotlin/Open_keyword.kt
+++ src/main/kotlin/Open_keyword.kt
@@ -1,9 +1,17 @@
+
 open class People(age: Int, name: String) {
     init {
         println("My name is $name.")
         println("My age is $age")
     }
-
+    open var weight : Int = 0
+        get() = field
+        set(value) {
+            field = value
+        }
+    open fun displayWeight() : String {
+        return "Your weight is $weight kg!"
+    }
 }
 
 class MathTeacher(age: Int, name: String): People(age, name) {
@@ -12,7 +20,15 @@ class MathTeacher(age: Int, name: String): People(age, name) {
         println("I teach in primary school.")
     }
 
+    override var weight: Int = 0
+        get() = field
+        set(value) {
+            field = value + 10
+        }
 
+    override fun displayWeight(): String {
+        return "Your weight is ${weight + 10} kg"
+    }
 }
 
 class Footballer(age: Int, name: String): People(age, name) {
@@ -24,7 +40,9 @@ class Footballer(age: Int, name: String): People(age, name) {
 fun main() {
     val t1 = MathTeacher(25, "Jack")
     t1.teachMaths()
-
+    t1.weight = 50
+    println(t1.weight)
+    println(t1.displayWeight())
     println()
 
     val f1 = Footballer(29, "Christiano")
